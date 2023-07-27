3540df0832040d3e7d20a7e02b8aaf881ea37ab0
Asta Svirusiene <asta.svirusiene@accenture.com>
2023-07-27 23:25:46 +0300

    fix: mistakes

--- api/app.js
+++ api/app.js
@@ -5,9 +5,12 @@ const router = require('./src/router')
 const app = express()
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
+
 app.use('/', router);
-aoo.use(express.static(path.resolve(__dirname, 'uploads')));
-app.use('/*', (req, res) => {
-  res.sendFile(pathToIndex)
+
+app.use(express.static(path.resolve(__dirname, 'uploads')));
+app.use('/*', (request, response) => {
+  response.sendFile(pathToIndex)
 });
+
 module.exports = app;
--- api/src/resizeWorker.js
+++ api/src/resizeWorker.js
@@ -3,8 +3,8 @@ const {parentPort, workerData} = require('worker_threads')
 
 gm(workerData.source)
   .resize(100, 100)
-  .write(workerData.destination, (err) => {
-    if (err)
-      throw err
+  .write(workerData.destination, (error) => {
+    if (error) throw error;
+    
     parentPort.postMessage({resized: true})
 })
